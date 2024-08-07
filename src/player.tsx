7ad305da7832d12e8ae8ba889f0c604e4cb0a566
hassan34319 <hassaniqbal19343@gmail.com>
2024-08-07 08:41:36 +0500

    Removed Captions

--- src/player.tsx
+++ src/player.tsx
@@ -155,9 +155,6 @@ export function Player() {
             src="https://images.unsplash.com/photo-1486286701208-1d58e9338013?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
           />
-          {textTracks.map((track) => (
-            <Track {...track} key={track.src} />
-          ))}
         </MediaProvider>
 
         <VideoLayout
